5ffa034192fd47439d14286d59880650fb2f3037
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-08-03 07:16:07 +0000

    gem追加とdevise関連の追加

---
+++ app/models/artist.rb
@@ -0,0 +1,14 @@
+class Artist < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |artist|
+      artist.password = SecureRandom.urlsafe_base64
+      artist.password_confirmation = artist.password
+      artist.name = "ゲストアーティスト"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :admins,skip:[:passwords, :registrations], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :gallaries,skip: [:passwords],controllers: {
+    registrations: "gallary/registrations",
+    sessions: "gallary/sessions"
+  }
+  devise_scope :gallaries do
+    post 'gallary/guest_sign_in',to: 'gallary/sessions#guest_sign_in'
+  end
+  devise_for :artists,skip: [:passwords],controllers: {
+    registrations: "artist/registrations",
+    sessions: "artist/sessions"
+  }
+  devise_scope :artists do
+    post 'artist/guest_sign_in', to: 'artist/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

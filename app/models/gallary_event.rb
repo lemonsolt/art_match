b6c3c6fc179b0cff75c52feeed6997b53cdd80d1
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-10-28 13:25:33 +0000

    [fix]

--- app/models/gallary_event.rb
+++ app/models/gallary_event.rb
@@ -26,7 +26,7 @@ class GallaryEvent < ApplicationRecord
   end
   
   def start_at_must_be_before_end_at
-    if start_at && end_at && start_at > end_at
+    if start_at > end_at
       errors.add(:start_at, "は終了日時よりも前でなければなりません")
     end
   end

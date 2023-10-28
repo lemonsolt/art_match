141a7edcd3ec0ad80b55f3ce01a21c1dcc5a88f9
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-10-28 13:12:23 +0000

    [update]enum自動変更

--- app/models/gallary_event.rb
+++ app/models/gallary_event.rb
@@ -12,6 +12,11 @@ class GallaryEvent < ApplicationRecord
   enum recruit:{
     募集終了:0,募集中:1,開催中:2,展示終了:3
   }
+  
+  before_save :update_recruit_status_start_at
+  before_save :update_recruit_status_end_at
+  
+  private
 
   def image_or_empty
     if self.image.attached? == false
@@ -24,6 +29,18 @@ class GallaryEvent < ApplicationRecord
   def start_at_must_be_before_end_at
     if start_at && end_at && start_at > end_at
       errors.add(:start_at, "は終了日時よりも前でなければなりません")
+    end
+  end
+  
+  def update_recruit_status_start_at
+    if start_at == Date.today
+      self.recruit = :開催中
+    end
+  end
+  
+  def update_recruit_status_end_at
+    if end_at < Date.today
+      self.recruit = :展示終了
     end
   end
 
